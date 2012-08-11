9983124971258c7bc32334af9227147b6bfff323
unknown <chulwoo@chulwoo-PC.(none)>
2012-08-11 16:14:18 +0900

    first commit..

---
+++ src/chulwoo/kim/MyPhoneGapActivity.java
@@ -0,0 +1,16 @@
+package chulwoo.kim;
+
+import org.apache.cordova.DroidGap;
+import org.apache.cordova.api.LOG;
+
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		long startTme=System.currentTimeMillis();
+		super.loadUrl("file:///android_asset/www/index.html");
+		LOG.e("TIME", "loading page costs "+(System.currentTimeMillis()-startTme) +"milli.");
+	}
+}
